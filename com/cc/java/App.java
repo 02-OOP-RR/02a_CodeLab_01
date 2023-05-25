494e55bfe052cb638c6476d5075f8f330522e8a5
Roxana <roxanarevilla@outlook.de>
2023-05-25 16:50:27 +0200

    Übung_Konto_Lösung

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -1,17 +1,37 @@
 package com.cc.java;
 
 public class App {
-    
+
     public static void main(String[] args) {
-    
+        
        
-    }
+        Konto konto1 = new Konto(10000);
+        Konto konto2 = new Konto(10000);
+        Konto konto3 = new Konto(10000);
+    
+            output("konto1: " + konto1.getKontostand());
+            output("konto2: " + konto2.getKontostand());
+            output("konto3: " + konto3.getKontostand());
+    
+            output("-------------");
+
+        konto1.setKontostand(konto1.getKontostand()*2);
+        konto2.setKontostand(konto2.getKontostand()*3);
+        konto3.setKontostand(konto3.getKontostand()*10);
+
+        output("Konto1: " + konto1.getKontostand() );
+        output("Konto2: " + konto2.getKontostand ());
+        output("konto3: " + konto3.getKontostand());
+
+    
+}
 
 
-    public static void output(String outputStr) {
+    private static void output(String outputStr) {
         System.out.println(outputStr);
     } 
 
 
 }
+
 
